ae92b7f04e5bfa35e5a86e9e591013c6fdc6e5dc
Potato-Tomato-dev <82814551+Potato-Tomato-dev@users.noreply.github.com>
2021-11-28 16:57:43 +0800

    Add files via upload

---
+++ src/layout/Bar.js
@@ -0,0 +1,28 @@
+// import {Link} from "react-router-dom";
+
+
+// function C_2(){
+//     <Link to="/Section2"/>
+// }
+// function C_3(){
+//     <Link to="/Section3"/>
+// }
+// function C_4(){
+//     <Link to="/Section4"/>
+// }
+// function C_5(){
+//     <Link to="/Section5"/>
+// }
+
+function Bar (){
+    return(
+        <div className="Bar">
+        <button> Section1</button>
+        <button >Section 2</button>
+        <button >Section 3</button>
+        <button >Section 4</button>
+        <button >Section 5</button>
+        </div>
+    )
+}
+export default Bar
